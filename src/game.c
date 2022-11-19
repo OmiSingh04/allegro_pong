670502b3ed7a1986f322505914dc1237b3c58bda
OmiSingh04 <omisinghdbz@gmail.com>
2022-11-19 10:18:02 +0530

    i guess a better collision system

--- src/game.c
+++ src/game.c
@@ -1,10 +1,6 @@
 #include "game.h"
-
 #include "sound.h"
-
-
-#define BAR_VELOCITY  5.0
-
+#define BAR_VELOCITY  6.0
 
 bool detect_point(Ball ball, Player *player1, Player *player2, Dimension screen_dimensions){
 
@@ -42,34 +38,34 @@ void move_bar(Bar* bar, enum y_direction direction, unsigned int screen_height){
 
 void update_ball(Ball *ball, Bar bar1, Bar bar2, Dimension screen_dimensions){
 
+	//simply move the ball! but with twists
 	ball->position.x += ball->velocity.x;
 	ball->position.y += ball->velocity.y;
 
-	//up down collision
-	if(ball->position.y <= 0 || ball->position.y >= screen_dimensions.height){
-		ball->velocity.y *= -1;
-		play_collide();
-	}
+
+	//within screen dimensions check - 
 	
-
-	//bar collisions
-
-	//need to do the boolean checks again. reflects even without bar.
-	  
-	//bar1 and ball 	
-	if(bar1.position.x + bar1.size.width > ball->position.x && 
-		((ball->position.y + ball->radius > bar1.position.y) && (ball->position.y < bar1.position.y + bar1.size.width))){
-		ball->velocity.x *= -1;
+	//y within 0 and screen_height
+	if(ball->position.y < 0 || ball->position.y + ball->radius > screen_dimensions.height){
 		play_collide();
-	}
-		//i see the error now...
-
-	if(bar2.position.x > ball->position.x + ball->radius && 
-		/*y value cecks*/	(bar2.position.y < ball->position.y + ball->radius && bar2.position.y + bar2.size.height > ball->position.y)){
-		ball->velocity.x *= -1;
-		play_collide();
+		ball->velocity.y *= -1;
+		ball->position.y += (ball->velocity.y * 5);
 	}
 
+	//if x reaches the boundaries, then its a point, i think i will handle that in detect_point function
 
+	//now just the bar checks! i need a basic but not crappy collision detection how hard can that be...
+	if((ball->position.x <= bar1.position.x + bar1.size.width) && ((ball->position.y + ball->radius > bar1.position.y) && (ball->position.y - ball->radius < bar1.position.y + bar1.size.height))){
+		play_collide();
+		ball->velocity.x *= -1;
+		ball->position.x += 5;
+	}
+
+	if(ball->position.x + ball->radius > bar2.position.x && 
+		((ball->position.y + ball->radius > bar2.position.y) && (ball->position.y - ball->radius < bar2.position.y + bar2.size.height))){
+		play_collide();
+		ball->velocity.x *= -1;
+		ball->position.x -= 5;
+	}
 
 }
--- src/run.c
+++ src/run.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include <allegro5/allegro5.h>
 
 
@@ -11,14 +12,25 @@
 #include "sound.h"
 #define KEY_SEEN 1
 #define KEY_RELEASED 2
-
-
 #define DISPLAY_WIDTH 1000
 #define DISPLAY_HEIGHT 800
-
-#define BALL_RADIUS 25
-
+#define BALL_RADIUS 15
 #define CENTRE_LINE_WIDTH 5
+
+
+Vector2 *generate_stars(){
+	Vector2* vectors = malloc(sizeof(float) * 2 * 40);
+	float* ptr = (float*) vectors;
+	for(int i = 0; i < 40; i++, ptr++){
+		float x = rand() % DISPLAY_WIDTH;
+		float y = rand() % DISPLAY_HEIGHT;
+		*ptr = x;
+		*(++ptr) = y;
+	}
+	return vectors;
+	
+}
+
 
 void must_init(bool test, const char *message){
 	if(test)
@@ -41,11 +53,12 @@ int main(){
     	al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
     	al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
 	
+
 	Dimension display_dimensions = {.width = 1000, .height = 800};
 
 	ALLEGRO_DISPLAY* disp = al_create_display(display_dimensions.width, display_dimensions.height);
     	must_init(disp, "display");
-	
+		
 	must_init(al_init_primitives_addon(), "primitives");//drawing basic shapes and lines for my pong game
 
 	must_init(al_install_audio(), "audio");
@@ -57,22 +70,21 @@ int main(){
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	
 	init_sound_files();
-	printf("meow\n");
 
 
 	Bar bar1 = {.position = {.x = 100, .y = 640 / 2 - 20 / 2}, .size = {.width = 20, .height = 100}, .color = al_map_rgb(255, 192, 203)};
 	Bar bar2 = {.position = {.x = 900 - 20, .y = 640 / 2 - 20 / 2}, .size = {.width = 20, .height = 100}, .color = al_map_rgb(255, 192, 203)};
-	Ball ball = {.position = {.x = DISPLAY_WIDTH/2 - BALL_RADIUS/2, .y = DISPLAY_HEIGHT/2 - BALL_RADIUS/2}, {.x = -3.0, .y = 5.0}, BALL_RADIUS, al_map_rgb(255, 0, 0)};
+	Ball ball = {.position = {.x = DISPLAY_WIDTH/2 - BALL_RADIUS/2, .y = DISPLAY_HEIGHT/2 - BALL_RADIUS/2}, .velocity = {.x = -4, .y = -7}, BALL_RADIUS, al_map_rgb(255, 0, 0)};
 	bool game_over = false;
 	bool redraw = false;
 	ALLEGRO_EVENT event;
 	enum y_direction direction = UP;
 
-
 	unsigned char key[ALLEGRO_KEY_MAX];
 	memset(key, 0, sizeof(key));
+	
 
-
+	Vector2* stars = generate_stars();
 
 	al_start_timer(timer);//generates events, as the timer increments at a constant rate
 	while(!game_over){
@@ -113,16 +125,31 @@ int main(){
 			case ALLEGRO_EVENT_KEY_UP:
 				key[event.keyboard.keycode] &= KEY_RELEASED;
 				break;
+
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				game_over = true;
 		}
 
 		
 		update_ball(&ball, bar1, bar2, display_dimensions);
 		if(redraw){
+				
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 
-			//DRAW A THICCC LINE RIGHT IN THE MIDDLE
+			{//drawing them stars
+
+				Vector2* ptr = stars;
+				for(int i = 1; i <= 40; i++){
+					al_draw_filled_circle(ptr->x, ptr->y, rand()%4, al_map_rgb(255, 255, 255));//oh wow its like playing a game among twinkling stars
+					ptr++;
+				}
+
+			}
+			
 			al_draw_line(DISPLAY_WIDTH/2 - CENTRE_LINE_WIDTH/2, 0, DISPLAY_WIDTH/2 - CENTRE_LINE_WIDTH/2,
 					DISPLAY_HEIGHT,  al_map_rgb(255, 255, 255), CENTRE_LINE_WIDTH);
+
+			//DRAW A THICCC LINE RIGHT IN THE MIDDLE
 			
 
 			al_draw_filled_rectangle(bar1.position.x, bar1.position.y, bar1.position.x + bar1.size.width,
